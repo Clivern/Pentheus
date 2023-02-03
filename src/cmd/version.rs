5418a3789e34a82f037f2adb7fb495056d6b861f
Clivern <hello@clivern.com>
2023-02-03 21:14:49 +0200

    release v0.1.1

--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -3,10 +3,10 @@
 // license that can be found in the LICENSE file.
 
 pub fn get_version() -> &'static str {
-    "v0.1.0"
+    "v0.1.1"
 }
 
 #[test]
 fn test_get_version() {
-    assert_eq!(get_version(), "v0.1.0");
+    assert_eq!(get_version(), "v0.1.1");
 }
